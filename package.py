28ac59d26bf62e87bf2ea756075fbffb1118b02f
Simon Lambin <slambin@artfx.fr>
2021-09-21 09:56:02 +0200

    Initial commit

---
+++ package.py
@@ -0,0 +1,25 @@
+# pylint: skip-file
+name = "silex_maya"
+version = "0.1.0"
+
+authors = ["ArtFx TD gang"]
+
+description = \
+    """
+    Set of python module and maya config to integrate maya in the silex pipeline
+    Part of the Silex ecosystem
+    """
+
+vcs = "git"
+
+requires = ["silex_client", "maya", "python-3.7"]
+
+build_command = "python {root}/script/build.py {install}"
+
+def commands():
+    """
+    Set the environment variables for silex_maya
+    """
+    env.SILEX_ACTION_CONFIG = "{root}/config/action"
+    env.PYTHONPATH.append("{root}")
+    env.PYTHONPATH.append("{root}/startup")
---
+++ script/build.py
@@ -0,0 +1,47 @@
+import shutil
+import contextlib
+import sys
+import os
+
+
+def build(source_path, build_path, install_path, targets):
+    """
+    Build the rez package and install it if requested
+    """
+    src = os.path.join(source_path, "silex_maya")
+    config = os.path.join(source_path, "config")
+    startup = os.path.join(source_path, "startup")
+    package = os.path.join(source_path, "package.py")
+
+    # Clear the build folder, if a previous build were made
+    if os.path.exists(build_path):
+        with contextlib.suppress(FileNotFoundError):
+            shutil.rmtree(os.path.join(build_path, "silex_maya"))
+            shutil.rmtree(os.path.join(build_path, "config"))
+            shutil.rmtree(os.path.join(build_path, "startup"))
+            os.remove(os.path.join(build_path, "package.py"))
+
+    # Copy the source to the build location
+    shutil.copytree(src, os.path.join(build_path, "silex_maya"))
+    shutil.copytree(config, os.path.join(build_path, "config"))
+    shutil.copytree(startup, os.path.join(build_path, "startup"))
+    shutil.copy(package, build_path)
+
+    if "install" in (targets or []):
+        # Clear the install folder if a previous install where already here
+        if os.path.exists(install_path):
+            with contextlib.suppress(FileNotFoundError):
+                shutil.rmtree(install_path)
+
+        # Copy the source to the install location
+        shutil.copytree(src, os.path.join(install_path, "silex_maya"))
+        shutil.copytree(config, os.path.join(install_path, "config"))
+        shutil.copytree(startup, os.path.join(install_path, "startup"))
+        shutil.copy(package, install_path)
+
+
+if __name__ == '__main__':
+    build(source_path=os.environ['REZ_BUILD_SOURCE_PATH'],
+          build_path=os.environ['REZ_BUILD_PATH'],
+          install_path=os.environ['REZ_BUILD_INSTALL_PATH'],
+          targets=sys.argv[1:])
