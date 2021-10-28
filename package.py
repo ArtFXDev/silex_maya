e35303f6c89f010a219679c0195a959ee710bb11
Simon Lambin <slambin@artfx.fr>
2021-10-28 18:06:54 +0200

    Fix the package.py

--- package.py
+++ package.py
@@ -4,8 +4,7 @@ version = "0.1.0"
 
 authors = ["ArtFx TD gang"]
 
-description = \
-    """
+description = """
     Set of python module and maya config to integrate maya in the silex pipeline
     Part of the Silex ecosystem
     """
@@ -16,10 +15,11 @@ requires = ["silex_client", "maya", "python-3.7"]
 
 build_command = "python {root}/script/build.py {install}"
 
+
 def commands():
     """
     Set the environment variables for silex_maya
     """
-    env.SILEX_ACTION_CONFIG = "{root}/config/action"
+    env.SILEX_ACTION_CONFIG.prepend("{root}/config")
     env.PYTHONPATH.append("{root}")
     env.PYTHONPATH.append("{root}/startup")
