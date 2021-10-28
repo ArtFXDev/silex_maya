6557af0895ae4410e2dc6539d561d7c4c65a5282
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-10-28 15:15:04 +0200

    feature: export vrscene

--- silex_maya/commands/export_ass.py
+++ silex_maya/commands/export_ass.py
@@ -4,7 +4,7 @@ from typing import Any, Dict
 
 from silex_maya.utils.utils import Utils
 from silex_client.action.command_base import CommandBase
-from silex_client.utils.parameter_types import RangeParameterMeta
+from silex_client.utils.parameter_types import SelectParameterMeta
 
 # Forward references
 if typing.TYPE_CHECKING:
@@ -23,11 +23,19 @@ class Export_ass(CommandBase):
     Export selection as obj
     """
 
+    cam_list = cmds.listCameras()
+    cam_list.append('No camera')
+
     parameters = {
         "file_path": {
             "label": "File path",
             "type": pathlib.Path,
             "value": None,
+        },
+        "camera": {
+            "label": "Export camera",
+            "type": SelectParameterMeta(*cam_list),
+            "tooltip": "Name the render camera"
         },
         "selection": {
             "label": "Export selection",
@@ -40,45 +48,48 @@ class Export_ass(CommandBase):
         "bounding_box": {
             "label": "Export Bounding Box",
             "type": bool,
+            "value": True,
         },
         "binary_encoding": {
             "label": "Use Binary Encoding",
             "type": bool,
+            "value": True,
         },
 
-        "camera": {
-            "label": "Export camera",
-            "type": str,
-            "value": "",
-            "tooltip": "Name the render camera"
-        },
         "options": {
             "label": "Options",
             "type": bool,
+            "value": True,
         },
-        "light": {
+        "lights": {
             "label": "Lights",
             "type": bool,
+            "value": True,
         },
         "shapes": {
             "label": "shapes",
             "type": bool,
+            "value": True,
         },
         "shaders": {
             "label": "Shaders",
             "type": bool,
+            "value": True,
         },
         "override": {
             "label": "Override Nodes",
             "type": bool,
+            "value": True,
         },
         "diver": {
             "label": "Divers",
             "type": bool,
+            "value": True,
         },
         "filters": {
             "label": "Filters",
             "type": bool,
+            "value": True,
         },
     }
 
@@ -86,8 +97,19 @@ class Export_ass(CommandBase):
     async def __call__(
         self, upstream: Any, parameters: Dict[str, Any], action_query: ActionQuery
     ):
+
         def compute_mask():
-            pass
+            options = parameters.get('options')
+            camera = bool(parameters.get('camera') != 'No camera')
+            light = parameters.get('lights')
+            shape = parameters.get('shapes')
+            shader = parameters.get('shaders')
+            override = parameters.get('override')
+            diver = parameters.get('diver')
+            filters = parameters.get('filters')
+
+            return 1*options + 2*camera + 4*light + 8*shape + \
+                16*shader + 32*override + 64*diver + 128*filters
 
         path = parameters.get('file_path')
         sel = parameters.get('selection')
@@ -95,6 +117,7 @@ class Export_ass(CommandBase):
         Slinks = parameters.get('light')
         Bbox = parameters.get('bounding_box')
         cam = parameters.get('camera')
+        binary = parameters.get('binary_encoding')
         mask = compute_mask()
 
         p = pathlib.path(path)
@@ -107,6 +130,7 @@ class Export_ass(CommandBase):
             lightLinks=Llinks,
             shadowLinks=Slinks,
             boundingBox=Bbox,
+            asciiAss=bool(1-binary),
             mask=mask,
         )
 
---
+++ silex_maya/commands/export_vrscene.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+import typing
+from typing import Any, Dict
+
+from silex_maya.utils.utils import Utils
+from silex_client.action.command_base import CommandBase
+from silex_client.utils.parameter_types import SelectParameterMeta
+
+# Forward references
+if typing.TYPE_CHECKING:
+    from silex_client.action.action_query import ActionQuery
+
+from silex_maya.utils.dialogs import Dialogs
+from silex_maya.utils.utils import Utils
+
+import maya.cmds as cmds
+import os
+import pathlib
+
+
+class Export_vrscene(CommandBase):
+    """
+    Export selection as obj
+    """
+
+    cam_list = cmds.listCameras()
+    cam_list.append('No camera')
+
+    parameters = {
+        "file_path": {
+            "label": "File path",
+            "type": pathlib.Path,
+            "value": None,
+        }
+    }
+
+    @ CommandBase.conform_command()
+    async def __call__(
+        self, upstream: Any, parameters: Dict[str, Any], action_query: ActionQuery
+    ):
+
+        path = parameters.get('file_path')
+
+        cmds.file(path, options=True, force=True,
+                  pr=True, ea=True, typ="V-Ray Scene")
+
+        if os.path.exists(path):
+            Dialogs.inform('Export SUCCEDE !')
+        else:
+            Dialogs.error('ERROR : Export FAILD !')
