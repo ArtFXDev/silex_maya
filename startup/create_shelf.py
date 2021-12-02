21d12f2cd229a9591944fe88e335bc692d53d3b5
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-12-02 12:24:10 +0100

    feature: redirect maya save to silex save

---
+++ startup/create_shelf.py
@@ -0,0 +1,32 @@
+import maya.cmds as cmds
+
+def create_shelf():
+    shelf_id = "silex_shelf"
+
+    if cmds.shelfLayout(shelf_id, ex=1):
+        if cmds.shelfLayout(shelf_id, q=1, ca=1):
+            for each in cmds.shelfLayout(shelf_id, q=1, ca=1):
+                cmds.deleteUI(each)
+        cmds.deleteUI(shelf_id)
+
+    cmds.shelfLayout(shelf_id, p="ShelfLayout")
+    cmds.setParent(shelf_id)
+
+    import_statement = "from silex_client.action.action_query import ActionQuery\n"
+    actions = {
+        item["name"]: f"{import_statement}ActionQuery('{item['name']}').execute()"
+        for item in Config().actions
+    }
+
+    for action in actions:
+        cmds.setParent(shelf_id)
+        cmds.shelfButton(
+            width=37,
+            height=37,
+            image="commandButton.png",
+            l=action,
+            imageOverlayLabel=action,
+            command=actions[action],
+            olb=(0, 0, 0, 0),
+            olc=(0.9, 0.9, 0.9),
+        )
---
+++ startup/custom_save.py
@@ -0,0 +1,16 @@
+import maya.cmds as cmds
+import maya.mel as mel
+
+def custom_save():
+    # no install if in batch mode
+    if cmds.about(batch=True):
+        return
+    cmd='python("from silex_client.action.action_query import ActionQuery;ActionQuery(\'save\').execute()")'
+    # hijack save button
+    cmds.iconTextButton(u"saveSceneButton", edit=True, command=cmd, sourceType="mel")
+    # hijack save menu item
+    mel.eval("buildFileMenu();") # new in Maya 2009, we have to explicitly create the file menu before modifying it
+    cmds.setParent(u"mainFileMenu", menu=True)
+    cmds.menuItem(u"saveItem", edit=True, label="Save Scene", command=cmd)
+    # hijack CTRL-S named command
+    cmds.nameCommand(u"NameComSave_File", annotation="Your Custom Save", command=cmd )
