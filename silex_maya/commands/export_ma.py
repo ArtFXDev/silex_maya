b48999dcb7a1ef390c4c594dd30333db8cbc08e6
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2021-12-14 14:58:59 +0100

    feature: publish ma scenes

--- silex_maya/commands/export_ma.py
+++ silex_maya/commands/export_ma.py
@@ -3,13 +3,14 @@ import typing
 from typing import Any, Dict
 
 from silex_client.action.command_base import CommandBase
-from silex_client.utils.parameter_types import RangeParameterMeta
+from silex_client.action.parameter_buffer import ParameterBuffer
+from silex_client.utils.parameter_types import TextParameterMeta
+
+from silex_maya.utils.utils import Utils
 
 # Forward references
 if typing.TYPE_CHECKING:
     from silex_client.action.action_query import ActionQuery
-
-from silex_maya.utils.utils import Utils
 
 import maya.cmds as cmds
 import os
@@ -22,7 +23,7 @@ class ExportMa(CommandBase):
     """
 
     parameters = {
-        "file_dir": {
+        "directory": {
             "label": "File directory",
             "type": pathlib.Path,
             "value": None,
@@ -31,27 +32,44 @@ class ExportMa(CommandBase):
             "label": "File name",
             "type": pathlib.Path,
             "value": None,
-        },
-        "range": {
-            "label": "Range",
-            "type": RangeParameterMeta(1, 475, 1),
-        },
+        }
     }
+
+    async def _prompt_select_parameter(
+        self, action_query: ActionQuery
+    ) -> bool:
+        """
+        Helper to prompt the user a label
+        """
+        # Create a new parameter to prompt label
+
+        info_parameter = ParameterBuffer(
+            type=TextParameterMeta('warning'),
+            name="Info",
+            label="Info",
+            # value=f"WARNING: No selection detected -> Please selecte somthing or publish full scene",
+        )
+        bool_parameter = ParameterBuffer(
+            type=bool,
+            name="full_scene",
+            label='Publish full scene',
+            value=True
+        )
+        # Prompt the user with a label
+        prompt: Dict[str, ANy] = await self.prompt_user(action_query, {"info": info_parameter, 'full_scene': bool_parameter})
+
+        return prompt['full_scene']
+
 
     @CommandBase.conform_command()
     async def __call__(
         self, parameters: Dict[str, Any], action_query: ActionQuery, logger: logging.Logger
     ):
 
-        def export_ma(path: str) -> None:
-
-            if not len(cmds.ls(sl=True)):
-                raise Exception('ERROR: No selection detected')
-
-            cmds.file(path, exportSelected=True, pr=True, typ="mayaAscii")
-
-        directory: str = str(parameters.get("file_dir"))
+       
+        directory: str = str(parameters.get("directory"))
         file_name: str = str(parameters.get("file_name"))
+        full_scene: bool = False
         
         # Check for extension
         if "." in file_name:
@@ -59,11 +77,19 @@ class ExportMa(CommandBase):
           
         export_path: str = f"{directory}{os.path.sep}{file_name}.ma"
 
-        # Export the selection in OBJ
+        logger.info('export')
+
+        # Export the selection in ma
+        future: Any = await Utils.wrapped_execute(action_query, cmds.ls, sl=1)
+        slection_list: List[str] = await future
+
+        while not len(slection_list):
+            full_scene = await self._prompt_select_parameter(action_query)
+            future: Any = await Utils.wrapped_execute(action_query, cmds.ls, sl=1)
+            slection_list = await future
+
         os.makedirs(directory, exist_ok=True)
-
-
-        await Utils.wrapped_execute(action_query, lambda: export_ma(export_path))
+        cmds.file(export_path, ea=full_scene, exportSelected=not(full_scene), pr=True, typ="mayaAscii")
 
         # Test if the export worked
         import time
@@ -73,3 +99,19 @@ class ExportMa(CommandBase):
             raise Exception(
                 f"An error occured while exporting {export_path} to ma")
         return export_path
+
+
+    async def setup(
+        self,
+        parameters: Dict[str, Any],
+        action_query: ActionQuery,
+        logger: logging.Logger,
+    ):
+        if 'info' in parameters:
+            if parameters.get("full_scene", False):            
+                self.command_buffer.parameters["info"].type = TextParameterMeta('warning')
+                self.command_buffer.parameters["info"].value = "WARNING: No selection detected -> Please selecte somthing or publish full scene"
+            else:
+                self.command_buffer.parameters["info"].type = TextParameterMeta('info')
+                self.command_buffer.parameters["info"].value = 'Select somthing to publish'
+
