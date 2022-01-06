b91802c03fd71c200a625968b5632effeab5b47d
OlivierArgentieri <olivierargentieri@gmail.com>
2022-01-06 16:33:04 +0100

    fix #87

--- startup/userSetup.py
+++ startup/userSetup.py
@@ -3,9 +3,11 @@ import maya
 
 from custom_save import custom_save
 from create_shelf import create_shelf
+from load_plugins import load_plugins
 
 
 Context.get().start_services()
 maya.utils.executeDeferred(create_shelf)
 maya.utils.executeDeferred(custom_save)
+maya.utils.executeDeferred(load_plugins)
 
