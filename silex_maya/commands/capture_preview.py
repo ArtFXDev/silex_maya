e39bd2207764df4d392046153e0c004dc038ff7c
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2022-01-12 18:01:56 +0100

    feature: upload preview

--- silex_maya/commands/capture_preview.py
+++ silex_maya/commands/capture_preview.py
@@ -2,10 +2,15 @@ from __future__ import annotations
 import typing
 from typing import Any, Dict
 import logging
+from maya import cmds
+from maya import mel  
+import tempfile
+import uuid
 
+from typing import Optional
 from silex_client.action.command_base import CommandBase
 from silex_maya.utils.utils import Utils
-from silex_maya.utils.preview import create_thumbnail
+
 
 # Forward references
 if typing.TYPE_CHECKING:
@@ -16,13 +21,47 @@ class CapturePreview(CommandBase):
     """
     Capture the current viewport either as a playblast or a single frame
     """
+    
+    
+    def create_thumbnail(self) -> Optional[str]:
+    
+        # 6 name thumbnail
+        file_name = str(uuid.uuid1())
+
+        # export path
+        tmp_path = str(tempfile.gettempdir()) + "\\{}".format(file_name)
+        tmp_path = tmp_path.replace('\\', '/')
+
+        # get current frame
+        current_frame = int(cmds.currentTime(query=True))
+
+        default = ["front", "persp", "side", "top"]
+        cam_lst = cmds.listCameras()
+        cam_lst = list(set(cam_lst) - set(default))
+
+        width = 1080 or int(cmds.getAttr("defaultResolution.width"))
+        height = 720 or int(cmds.getAttr("defaultResolution.height"))
+        deviceAspectRatio = width / float(height)
+
+        if len(cam_lst):
+            cmds.lookThru(cam_lst[0])
+            cmds.setAttr("defaultResolution.deviceAspectRatio", deviceAspectRatio)
+            mel.eval(f'playblast  -format image -filename "{tmp_path}" -clearCache 1 -viewer 0 -frame 1 -showOrnaments 0 -percent 50 -compression "jpg" -quality 70 -widthHeight {width} {height};')
+            return f"{tmp_path}.0000.jpg"
+
+        cmds.setAttr("defaultResolution.deviceAspectRatio", deviceAspectRatio)
+        mel.eval(f'playblast  -format image -filename "{tmp_path}" -clearCache 1 -viewer 0 -frame 1 -showOrnaments 0 -percent 50 -compression "jpg" -quality 70 -widthHeight {width} {height};')
+
+
+        return f"{tmp_path}.0000.jpg"
+
 
     @CommandBase.conform_command()
     async def __call__(
         self, parameters: Dict[str, Any], action_query: ActionQuery, logger: logging.Logger
     ):
         # Take a thumbnail of the current viewport
-        thumbnail_future = await Utils.wrapped_execute(action_query, create_thumbnail)
-        thumbnail = await thumbnail_future
+        thumbnail_future = await Utils.wrapped_execute(action_query, self.create_thumbnail)
+        thumbnail = thumbnail_future.result()
 
         return thumbnail
--- silex_maya/commands/upload_preview.py
+++ silex_maya/commands/upload_preview.py
@@ -9,7 +9,6 @@ import logging
 
 from silex_client.action.command_base import CommandBase, CommandParameters
 from silex_client.utils.log import logger
-from silex_client.utils.parameter_types import SelectParameterMeta
 
 # Forward references
 if typing.TYPE_CHECKING:
@@ -22,13 +21,7 @@ class UploadPreview(CommandBase):
     """
 
     parameters: CommandParameters = {
-        "preview_path": {"label": "Preview path", "type": str, "value": None},
-        "description": {"label": "Description", "type": str, "value": None},
-        "status": {
-            "label": "Task Status",
-            "type": SelectParameterMeta("wfa", "done", "wip", "retake"),
-            "value": None,
-        },
+        "preview_path": {"label": "Preview path", "type": pathlib.Path, "value": None},
     }
 
     required_metadata: List[str] = ["task_id"]
@@ -37,6 +30,7 @@ class UploadPreview(CommandBase):
     async def __call__(
         self, parameters: Dict[str, Any], action_query: ActionQuery, logger: logging.Logger
     ):
+
         if not os.path.isfile(parameters["preview_path"]):
             logger.error(
                 "The preview file path %s is incorrect", parameters["preview_path"]
@@ -53,21 +47,22 @@ class UploadPreview(CommandBase):
         # Check the given task status
         task_statuses = await gazu.task.all_task_statuses()
         task_status = await gazu.task.get_task_status(task["task_status"]["id"])
-        if parameters["status"] in [
-            task_status["short_name"] for task_status in task_statuses
-        ]:
-            task_status = await gazu.task.get_task_status_by_short_name(
-                parameters["status"]
-            )
-        else:
-            logger.warning(
-                "Could not set the given task status: The given task status %s is invalid",
-                parameters["status"],
-            )
 
-        await gazu.task.add_comment(
-            task,
-            task_status,
-            parameters["description"],
-            attachments=[parameters["preview_path"]],
+        # add comment with preview
+        comment = await gazu.task.add_comment(task, task_status)
+
+        # add preview
+        preview_file = await gazu.task.add_preview(
+        task,
+        comment,
+        parameters["preview_path"]
         )
+        import time
+        time.sleep(10)
+        # set preview
+        logger.error(preview_file)
+        import gazu.client as raw
+        await raw.put(f"actions/preview-files/{preview_file['id']}/set-main-preview",{})
+
+        # await gazu.task.set_main_preview(preview_file['id'])
+
