0e374e0d8b56fd9d1f3bd100f238cd8a576b0de3
OlivierArgentieri <olivierargentieri@gmail.com>
2022-01-13 17:49:53 +0100

    remove prompt=False

--- silex_maya/commands/save.py
+++ silex_maya/commands/save.py
@@ -30,7 +30,7 @@ class Save(CommandBase):
     ):
         def save(file_path: str):
             cmds.file(rename=file_path)
-            cmds.file(save=True, type="mayaAscii", prompt=False)
+            cmds.file(save=True, type="mayaAscii")
 
         file_path = parameters["file_path"]
         logger.info(file_path)
