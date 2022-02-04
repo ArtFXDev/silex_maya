41357fcbee4c9c50edb30be646b773680e18d34a
Simon Lambin <slambin@artfx.fr>
2022-02-04 10:26:03 +0100

    Handle <frame> syntax

---
+++ silex_maya/utils/constants.py
@@ -0,0 +1,7 @@
+import re
+
+# Regex used to expand and capture syntaxes for sequences like #### or <UDIM> ...
+MATCH_FILE_SEQUENCE = [
+    re.compile(r"^.+\W(\<.+\>)\W.+$"),  # Matches V-ray's <Whatever> syntax
+    re.compile(r"^.+[^\w#](#+)\W.+$"),  # Matches V-ray's ####  syntax
+]
