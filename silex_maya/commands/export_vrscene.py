cd067f38d76ee98b3c69a4d9cda3418e8d9555a0
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2022-02-10 12:42:41 +0100

    fix vrayformaya

--- silex_maya/commands/export_vrscene.py
+++ silex_maya/commands/export_vrscene.py
@@ -46,18 +46,6 @@ class ExportVrscene(CommandBase):
         },
     }
 
-    def save_as_temp(self, scene_path: pathlib.Path, temp_directory: pathlib.Path) -> pathlib.Path:
-
-            temp_scene = (temp_directory / f'{scene_path.stem}_temp').with_suffix(f'.ma')
-            cmds.file(rename=temp_scene)
-            cmds.file(save=True, type="mayaAscii")
-
-            # switch back to original scene
-            cmds.file(rename=scene_path)
-            cmds.file(save=True, type="mayaAscii")
-
-            return temp_scene
-
     @CommandBase.conform_command()
     async def __call__(
         self,
@@ -75,11 +63,7 @@ class ExportVrscene(CommandBase):
         output_files: List[pathlib.Path] = list()
         command_label = self.command_buffer.label
 
-
-        # Create temporary scene for rendering (in case opened scene is empty or named incorrectly)
-        active_scene: str = await thread_maya.execute_in_main_thread(cmds.file, q=True, sn=True)
-        temp_scene: pathlib.Path = await thread_maya.execute_in_main_thread(self.save_as_temp, pathlib.Path(active_scene), directory)
-
+        render_scene: str = await thread_maya.execute_in_main_thread(cmds.file, q=True, sn=True)
 
         for index, layer in enumerate(render_layers):
 
@@ -102,7 +86,7 @@ class ExportVrscene(CommandBase):
                 .param("rl", layer)
                 .param("exportFileName", str(output_path))
                 .param("noRender")
-                .value(str(temp_scene))
+                .value(str(render_scene))
             )
 
             await thread_client.execute_in_thread(
