75fdb288921ade1af1ef8122b50a9899b53e2df5
GuillaumeRoeder <55831311+GuillaumeRoeder@users.noreply.github.com>
2022-03-18 17:18:25 +0100

    feature ma bundle yaml

--- silex_maya/commands/save.py
+++ silex_maya/commands/save.py
@@ -32,15 +32,21 @@ class Save(CommandBase):
     ):
         file_path = parameters["file_path"]
 
+        logger.error(file_path)
+
         def save(file_path: str):
+            logger.error(os.environ.get('BUNDLE_FOLDER'))
+            logger.error(file_path)
             cmds.file(rename=file_path)
             cmds.file(save=True, type="mayaAscii")
 
         if os.path.splitext(file_path)[1] != ".ma":
             file_path = f"{os.path.splitext(file_path)[0]}.ma"
+        logger.error(file_path)
 
         logger.info("Saving scene to %s", file_path)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        logger.error(file_path)
         await execute_in_main_thread(save, file_path=file_path)
 
         return {"new_path": file_path}
